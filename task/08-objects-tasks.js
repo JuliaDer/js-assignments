b46053ebcf261479677b746184cfd49149353db9
JuliaDer <39259792+JuliaDer@users.noreply.github.com>
2020-08-15 18:06:30 +0200

    Move line outside function

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -25,8 +25,12 @@
 function Rectangle(width, height) {
     this.width = width;
     this.height = height;
-    Rectangle.prototype.getArea = () => this.width * this.height;
 }
+
+Rectangle.prototype.getArea = function() {
+    return this.width * this.height;
+};
+
 
 
 /**
